b35d65f1082206fc67a81c8c99ab01e850508902
plago-bergonpazos <plago-bergonpazos@atanasoff14.danielcastelao.org>
2015-11-04 11:26:54 +0100

    commit1

---
+++ Boletín 7 Ej. 5/src/boletín/pkg7/ej/pkg5/Boletín7Ej5.java
@@ -0,0 +1,17 @@
+
+package boletín.pkg7.ej.pkg5;
+
+/**
+ *
+ * @author plago-bergonpazos
+ */
+public class Boletín7Ej5 {
+
+    
+    public static void main(String[] args) {
+        Numeros obx = new Numeros();
+        
+        obx.calculaMayor(obx.introduce(), obx.introduce(), obx.introduce());
+    }
+    
+}
