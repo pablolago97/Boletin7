02b0ce587ad4eb69bc19750c009e0136c6b9b52a
plago-bergonpazos <plago-bergonpazos@atanasoff14.danielcastelao.org>
2015-11-04 11:26:35 +0100

    commit1

---
+++ Boletín 7 Ej. 2/src/boletín/pkg7/ej/pkg2/Boletín7Ej2.java
@@ -0,0 +1,18 @@
+
+package boletín.pkg7.ej.pkg2;
+
+/**
+ *
+ * @author plago-bergonpazos
+ */
+public class Boletín7Ej2 {
+
+    
+    public static void main(String[] args) {
+        
+        Suma obx = new Suma();
+        
+        obx.comparar(obx.setNum(), obx.setNum2());
+    }
+    
+}
