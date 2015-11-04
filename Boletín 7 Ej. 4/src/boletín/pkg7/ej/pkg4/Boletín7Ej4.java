38655ccbf0bf27bfb2cc2fda7f0dea69ad703b7f
plago-bergonpazos <plago-bergonpazos@atanasoff14.danielcastelao.org>
2015-11-04 11:26:48 +0100

    commit1

---
+++ Boletín 7 Ej. 4/src/boletín/pkg7/ej/pkg4/Boletín7Ej4.java
@@ -0,0 +1,18 @@
+
+package boletín.pkg7.ej.pkg4;
+
+/**
+ *
+ * @author plago-bergonpazos
+ */
+public class Boletín7Ej4 {
+
+    public static void main(String[] args) {
+        
+        Peso obx = new Peso();
+        
+        obx.calcularPeso(obx.peso(), obx.peso(), obx.nombre(), obx.nombre());
+        
+    }
+    
+}
